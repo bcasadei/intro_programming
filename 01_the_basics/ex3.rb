384fd5df6e5dd7cfed38091f8428fcff965cea52
bcasadei <bill@billcasadei.com>
2016-04-05 20:57:31 -0600

    Chapter 1 - The Basics

---
+++ 01_the_basics/ex3.rb
@@ -0,0 +1,11 @@
+ movies = {
+  :movie1 => "1970",
+  :movie2 => "1984",
+  :movie3 => "1950",
+  :movie4 => "1990"
+}
+
+puts movies[:movie1]
+puts movies[:movie2]
+puts movies[:movie3]
+puts movies[:movie4]
---
+++ 01_the_basics/ex4.rb
@@ -0,0 +1,6 @@
+movieArr = ["1970", "1984", "1950", "1990" ]
+
+puts movieArr[0]
+puts movieArr[1]
+puts movieArr[2]
+puts movieArr[3]
